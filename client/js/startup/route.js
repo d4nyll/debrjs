0e9152ae180bfa691251146c16ec8cc1cb98e785
Daniel Li <dan@danyll.com>
2014-12-29 01:14:46 +0800

    Ensures routing is loaded first

---
+++ client/js/startup/route.js
@@ -0,0 +1,33 @@
+Router.configure({
+  layoutTemplate: 'app',
+  notFoundTemplate: '404'
+});
+
+Router.route('/', function () {
+	this.render('questionlist');
+	this.render('aside', {to: 'aside'});
+});
+
+Router.route('/error/:_id', function () {
+	this.render('error', {
+  		data: function() {
+  			return Errors.findOne({_id: parseInt(this.params._id, 10)});
+  		}
+	});
+});
+
+Router.route('/qa/', function () {
+	Router.go('/error/1/');
+});
+
+Router.route('/qa/:_qid', function () {
+	this.render('qa');
+	this.render('related', {to: 'aside'});
+	Session.set('editMode', false);
+});
+
+Router.route('/qa/:_id/edit/', function () {
+	this.render('qa');
+	this.render('related', {to: 'aside'});
+	Session.set('editMode', true);
+});
